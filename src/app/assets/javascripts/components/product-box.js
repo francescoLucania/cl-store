273f553605a9225bab9da0ef9baa510c903ad78f
francesco lucania <>
2018-04-01 16:55:48 +0300

    add product-box

---
+++ src/app/assets/javascripts/components/product-box.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    $(document).on('click', '.js-open-product-box', function () {
+
+        if(!$('.js-product-box').hasClass('is-active')) {
+
+            $('.js-page-shadow').addClass('is-active');
+
+            $('html').css('overflow', 'hidden');
+            $('body').css('overflow', 'hidden');
+
+            setTimeout(function () {
+
+                $('.js-product-box').addClass('is-active');
+
+            }, 1000)
+        }
+
+        return false
+
+    });
+
+    $(document).on('click', '.js-close-product-box', function () {
+
+        if($('.js-product-box').hasClass('is-active')) {
+
+            $('.js-product-box').removeClass('is-active');
+
+            setTimeout(function () {
+
+                $('.js-page-shadow').removeClass('is-active');
+
+                $('html').css('overflow', '');
+                $('body').css('overflow', '');
+
+            }, 600)
+        }
+
+        return false
+
+    });
+
+});
